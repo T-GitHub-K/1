7e541880be8df86ba45cb9d75973625456cc0eb3
T-GitHub-K <pmailwork-005@yahoo.co.jp>
2023-05-20 08:09:20 +0900

    conky1.10.07 CpuFan UP

---
+++ conky/script.lua
@@ -0,0 +1,20 @@
+function conky_cpuFan()
+    local result = tonumber(conky_parse("execi 1 sensors | grep 'Fan' | awk '{print $3}'"))
+    if result then
+        return result
+    else
+        return 0
+    end
+end
+
+function conky_cpuFanColor()
+    local cpuFanValue = tonumber(conky_parse("${execi 1 sensors | grep 'CPU Fan' | awk '{print $3}' | cut -d'.' -f1}"))
+
+    if cpuFanValue < 1000 then
+        return "${color green}"
+    elseif cpuFanValue < 2000 then
+        return "${color yellow}"
+    else
+        return "${color red}"
+    end
+end
